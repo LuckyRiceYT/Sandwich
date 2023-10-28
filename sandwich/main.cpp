61d199a7340d5e6c88284136900b1ce8cac8dbd1
[riceww] <93751666+LuckyRiceYT@users.noreply.github.com>
2023-10-28 09:31:57 -0500

    Sandwich v0.1
    official release of this shitpost lmao
    
    check "sandwichvariables.cpp" to see what one sandwich topping or stuffing can do to your code :3

---
+++ sandwich/main.cpp
@@ -0,0 +1,16 @@
+// Make sure to check "sandwichvariables.cpp" for the full list of variable names and what it does! :3
+// SANDWICH v0.1
+// A code shitpost by : LuckyRiceYT
+
+#include "sandwichvariables.cpp"
+#include <iostream>
+toothpick cucumber
+
+carrots sesameseeds mustard mustard2
+bread
+    chocolatespread name chickenspread "Rice" cucumber
+    tomato lettuce "Hello world!" lettuce avocado cucumber
+    tomato lettuce "My name is " lettuce name lettuce avocado cucumber
+    tomato lettuce "I am using Sandwich to code this simple console program :3" lettuce avocado cucumber
+    cheese 0 cucumber
+bread2
---
+++ sandwich/sandwichvariables.cpp
@@ -0,0 +1,27 @@
+// Sandwich v0.1
+// A code shitpost by : LuckyRiceYT
+#define bread {
+#define bread2 }
+#define carrots int
+#define peanutbutter float
+#define guavajelly char
+#define marmalade double
+#define chocolatespread string
+#define cheese return
+#define tomato cout
+#define avocado endl
+#define sesameseeds main
+#define mayonnaise cin
+#define egg >>
+#define lettuce <<
+#define cucumber ;
+#define chickenspread =
+#define toothpick using namespace std;
+#define mustard (
+#define mustard2 )
+#define margarine if
+#define jelly else if
+#define strawberryjam else
+#define jalapeno for
+#define birdseyechilli do
+#define bellpepper while
